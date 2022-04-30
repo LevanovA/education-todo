fa07464e69780c2ce915a0fa7101579f5034d428
Артем Леванов <tema.levanov@gmail.com>
2022-04-30 11:27:59 +0300

    some edits

--- src/index.js
+++ src/index.js
@@ -2,32 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import thunk from "redux-thunk";
-import {applyMiddleware, compose, createStore} from "redux";
-import {Provider} from "react-redux";
-import createSagaMiddleware from 'redux-saga'
-import {rootReducer} from "./store/rootReducer";
-import {logger} from "./store/middleware";
-import {sagasWatcher} from "./store/sagas";
+import thunk from 'redux-thunk';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { rootReducer } from './store/rootReducer';
+import { logger } from './store/middleware';
+import { sagasWatcher } from './store/sagas';
 
 const saga = createSagaMiddleware();
 
-const store = createStore(rootReducer, compose(
-    applyMiddleware(
-        logger, saga, thunk
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(logger, saga, thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
 saga.run(sagasWatcher);
 
 ReactDOM.render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
